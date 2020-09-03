b379fb4dc99172cc273a4ba94d2db62ce04331c8
LucasHarff <lucasharff@hotmail.com>
2020-09-02 22:15:20 -0300

    creat file

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./components/Header";
+import Container from "./components/Container";
+// import List from "./components/List";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Header title="To Do List - ReactJS App" />
+      <Container title="Inicia o body" />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Container.js
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+
+function Container() {
+  const [tasks, setTasks] = useState([
+    { newTask: "Estudar React", owner: "Luigi", simpleId: "testeUm1010" },
+  ]);
+  const [newTask, setNewTask] = useState(""); //string vazia
+  const [owner, setOwner] = useState("");
+
+  function addNewTask() {
+    const task = { newTask, owner, simpleId: `${Date.now()}` };
+
+    setTasks([...tasks, task]);
+    // console.log(tasks);
+    setNewTask("");
+    setOwner("");
+  }
+
+  return (
+    <div id="container">
+      <input
+        value={newTask}
+        placeholder="Digite uma nova tarefa"
+        type="text"
+        onChange={(e) => setNewTask(e.target.value)}
+      />
+      <br />
+      <input
+        value={owner}
+        placeholder="Digite o nome do criador"
+        type="text"
+        onChange={(e) => setOwner(e.target.value)}
+      />
+      <br />
+      <button type="button" onClick={addNewTask}>
+        Criar!
+      </button>
+      <br />
+
+      <div className="previewTask">
+        <h3>{newTask}</h3>
+        <h3>{owner}</h3>
+      </div>
+
+      <ul>
+        {tasks.map((eachTask) => (
+          <li key={eachTask.simpleId}>
+            <b>{eachTask.owner}: </b>
+            {eachTask.newTask}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Container;
